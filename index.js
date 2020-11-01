a833fcc27c25a68798d7c59d24bbe84838dc8896
Vincent Agnano <vinyll@protonmail.com>
2020-11-01 12:41:56 +0100

    button to delete an attestatioin

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-function prefill(data) {
+window.app = {
+  user: {},
+  attestations: [],
+  cardContainer: document.querySelector('#cards'),
+  cardTemplate: document.querySelector('#cards').innerHTML,
+}
+
+function prefillForm(data) {
   for (let p in data) {
     const input = document.querySelector(`[name="${p}"]`)
     if(input) input.value = data[p]
   }
 }
 
-if(location.search) prefill(queryParams())
-else prefill(JSON.parse(localStorage.user || '{}'))
-
 function loadAttestationsCards() {
-  const container = document.querySelector('#cards')
-  const template = container.innerHTML
-  const attestations = JSON.parse(localStorage.attestations || '[]')
-  const cards = attestations.map(data => {
-    return template
+  const cards = app.attestations.map(data => {
+    return app.cardTemplate
+      .replaceAll('{id}', data['id'])
       .replaceAll('{params}', serialize(data))
       .replaceAll('{reason}', document.querySelector(`[value=${data['field-reason']}]`).textContent)
       .replaceAll('{delay}', String(data['delay']))
   })
-  container.innerHTML = cards.join('')
+  app.cardContainer.innerHTML = cards.join('')
 }
-loadAttestationsCards()
+
+function deleteAttestation(id) {
+  app.attestations = app.attestations.filter(a => a.id != id)
+  saveAttestations()
+  loadAttestationsCards()
+}
 
 function submitData(form) {
   const data = Array.from(form.elements).filter(f => f.name).reduce((acc, field) => (Object.assign(acc, {[field.name]: field.value})), {})
@@ -36,9 +43,32 @@ function serialize(obj) {
   }).join('&')
 }
 
-function saveData(data) {
-  localStorage.user = JSON.stringify(data)
-  const attestations = JSON.parse(localStorage.attestations || '[]')
-  attestations.push(data)
-  localStorage.attestations = JSON.stringify(attestations)
+function loadData() {
+  attestations = JSON.parse(localStorage.attestations || '[]')
 }
+
+function saveAttestations() {
+  localStorage.attestations = JSON.stringify(app.attestations)
+}
+
+function saveUser() {
+  localStorage.user = JSON.stringify(app.user)
+}
+
+function saveData(data) {
+  app.user = JSON.stringify(data)
+  app.attestations.push(Object.assign({}, data, {id: Math.round(Math.random()*999999)}))
+  saveUser()
+  saveAttestations()
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  app.user = location.search
+    ? queryParams()
+    : JSON.parse(localStorage.user || '{}')
+  prefillForm(app.user)
+
+  app.attestations = JSON.parse(localStorage.attestations || '[]')
+
+  loadAttestationsCards()
+})
